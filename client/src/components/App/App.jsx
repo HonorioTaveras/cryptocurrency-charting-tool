7476fd14aee4033f5af73bbc5719994db3b34a95
Honorio <htaveras90@gmail.com>
2020-09-17 12:34:31 -0600

    added react-bootstrap, fetching api in client/logging in console successfully

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
 
 export default function App() {
-  return (
-    <div>hello world from App</div>
-  );
+  const [err, setErr] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [priceData, setPriceData] = useState([]);
+  const [currency, setCurrency] = useState('USD');
+
+  useEffect(() => {
+    Axios.get('https://api.coindesk.com/v1/bpi/currentprice.json').then(
+      (res) => {
+        console.log('res data: ', res.data);
+        setIsLoaded(true);
+        setPriceData(res.data);
+      },
+      // eslint-disable-next-line no-shadow
+      (err) => {
+        setIsLoaded(true);
+        setErr(err);
+      },
+    );
+  }, []);
+
+  return <div>hello world from App</div>;
 }
